ffd0ce101280233a6b49d4c3150976141a69b5c5
Sarthak <snegicodes@gmail.com>
2021-08-29 15:21:30 +0530

    adding todo functionality added

---
+++ app.js
@@ -0,0 +1,24 @@
+const addTodo = document.querySelector('.add');
+const todoList = document.querySelector('.todos')
+
+const createTodo = todo => {
+    const newTodo =
+    `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="far fa-trash-alt delete"></i>
+        </li>
+    `;
+    todoList.innerHTML += newTodo;
+};
+
+addTodo.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const todo = addTodo.add.value.trim();
+    if(todo.length){
+        createTodo(todo);
+        addTodo.reset();
+    }
+
+});
