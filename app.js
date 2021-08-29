4dc76e49d59d03627df995447c2c961c3ce50bbc
Sarthak <snegicodes@gmail.com>
2021-08-30 04:26:51 +0530

    Search Filter Added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const addTodo = document.querySelector('.add');
-const todoList = document.querySelector('.todos')
+const todoList = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
 const createTodo = todo => {
     const newTodo =
@@ -27,4 +28,23 @@ todoList.addEventListener('click', e => {
     if(e.target.classList.contains('delete')){
         e.target.parentElement.remove();
     }
+});
+
+const filterTodos = e => {
+
+    Array.from(todoList.children)
+    .filter((todo) =>!todo.textContent.includes(e))
+    .forEach((todo) => todo.classList.add('filtered'));
+
+    Array.from(todoList.children)
+    .filter((todo) => todo.textContent.includes(e))
+    .forEach((todo) => todo.classList.remove('filtered'));
+
+};
+
+search.addEventListener('keyup', () => {
+    
+    const key = search.value.trim();
+
+    filterTodos(key);
 });
